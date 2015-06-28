a3a1c9345bed0938a96bf9b173c47652d9ca7ee7
ayunav <ayunanyc@gmail.com>
2015-06-28 16:48:47 -0400

    third commit from Sunday

--- toDoListFromScratchZoufAndAyuna/main.m
+++ toDoListFromScratchZoufAndAyuna/main.m
@@ -12,50 +12,51 @@
 @class List;
 @class Item;
 
-//********* INTERFACE ITEM ********
+//************************************** INTERFACE ITEM ********
 @interface Item : NSObject
--(NSString*) setName;
--(int) setPriority;
 -(void) createItem: (NSString*) name withPriority: (int) itemPriority;
+-(NSString*) getName;
+-(int) getPriority;
 @end
 
-//********* IMPLEM ITEM ********
-
+//************************************** IMPLEM ITEM ********
 @implementation Item {
     NSString* _itemName;
     int _priority;
-}
--(NSString*) setName {
-    return _itemName;
-}
--(int) setPriority {
-    return _priority;
 }
 -(void) createItem: (NSString*) name withPriority: (int) itemPriority {
     _itemName = name;
     _priority = itemPriority;
 }
+-(NSString*) getName {
+    return _itemName;
+}
+-(int) getPriority {
+    return _priority;
+}
 @end
 
 
-
-//********* INTERFACE LIST ********
+//************************************** INTERFACE LIST ********
 @interface List : NSObject
+-(NSString*)listName;
+-(NSMutableArray*)items;
 -(void)setListNameAndInitializeItemsArray: (NSString*) listName;
 -(void) addItemToList: (Item*) task;
 -(void) printAllItemsInList;
-
 @end
 
-
-//********* IMPLEM LIST ********
-
+//************************************** IMPLEM LIST ********
 @implementation List {
     NSString* _listName;
     NSMutableArray* _items;
 }
-
-
+-(NSString*) getListName {
+    return _listName;
+}
+- (NSMutableArray*)items {
+    return _items;
+}
 -(void)setListNameAndInitializeItemsArray: (NSString*) listName {
     _listName = listName;
     _items = [[NSMutableArray alloc] init];
@@ -65,10 +66,10 @@
     [_items addObject:task];
 }
 
--(void) printAllItemsInList: (NSMutableArray*) itemsInAList {
+-(void) printAllItemsInList {
     for (int i = 0; i < [_items count]; i++) {
-        NSString *name = [[_items objectAtIndex:i] setName];
-        int priority = [[_items objectAtIndex:i] setPriority];
+        NSString *name = [[_items objectAtIndex:i] getName];
+        int priority = [[_items objectAtIndex:i] getPriority];
         NSLog(@"%@, %d", name, priority);
     }
 }
@@ -79,21 +80,56 @@
 @end
 
 
-//********* INTERFACE LIST MANAGER ********
+//************************************** INTERFACE LIST MANAGER ********
 @interface ListManager : NSObject
+-(NSMutableArray*) lists;
+-(void) addListToListManager: (List*) list;
+-(void)printLists;
 @end
 
-//********* IMPLEM LIST MANAGER ********
-
-@implementation ListManager
+//************************************** IMPLEM LIST MANAGER ********
+@implementation ListManager {
+    NSMutableArray *_lists;
+}
+- (NSMutableArray *) getLists {
+    if (_lists == nil) {
+        _lists = [[NSMutableArray alloc] init];
+    }
+    return _lists;
+}
+-(void) addListToListManager: (List*) list {
+    [_lists addObject:list];
+}
+-(void)printLists {
+    for (int i = 0; i < [_lists count]; i++) {
+        NSString *listName = [[_lists objectAtIndex:i] getListName];
+        NSLog(@"%@", listName);
+    }
+}
 @end
 
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Item *firstTask = [[Item alloc]init];
+        [firstTask createItem:@"Buy bananas" withPriority:1];
+        
+        Item *secondTask = [[Item alloc]init];
+        [secondTask createItem:@"Buy coffee and tea" withPriority:2];
+        
+        List *groceryList = [[List alloc]init];
+        [groceryList setListNameAndInitializeItemsArray:@"Grocery List"];
+        [groceryList addItemToList:firstTask];
+        [groceryList addItemToList:secondTask];
+        [groceryList printAllItemsInList];
+        
+        ListManager *oneListSoFar = [[ListManager alloc]init];
+        [oneListSoFar getLists];
+        [oneListSoFar addListToListManager: groceryList];
+        [oneListSoFar printLists];
+    
+    
     }
     return 0;
 }
